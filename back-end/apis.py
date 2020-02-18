726c065e8e3b5d6ec16123b0d55b2ac65bc0f344
Ubuntu <ubuntu@ip-172-31-32-228.ec2.internal>
2020-02-18 13:05:39 +0000

    added url in json response

--- back-end/apis.py
+++ back-end/apis.py
@@ -3,10 +3,14 @@ from db import db_connection
 
 def getBlogs():
     conn = db_connection()
-    print(conn)
+    #print(conn)
     cursor = conn.cursor()
+
+    sql = "SELECT user_data.author_name, user_content.title, user_content.content, user_content.url FROM user_data,user_content where user_data.id=user_content.author_id"
+    cursor.execute(sql)
+    output = cursor.fetchall()
 
     cursor.close()
     conn.close()
 
-    return {"status":1,"message":"Connection Successful"}
+    return {"status":1,"message":"Blog Data","response":output}
